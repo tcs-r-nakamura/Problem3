9790b725524fb72bef790ea4b3809c2829635bf8
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-03 10:38:26 +0900

    first commit

---
+++ Problem3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program3
+{
+    static void Main()
+    {
+        Random ran = new Random();
+        for (int i = 0; i < 6; i++)
+        {
+            int number = ran.Next(1, 44);
+            Console.WriteLine(number);
+        }
+    }
+}
