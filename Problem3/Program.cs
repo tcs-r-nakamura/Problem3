f993cd60bcb145ed235f8ebd4dab8008280eba55
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-07 09:21:03 +0900

    メソッド分割

--- Problem3/Program.cs
+++ Problem3/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+
 class Program3
 {
     static void Main()
     {
+        /* メソッドでランダムな数値を生成
+         * resultに重複しない６個の数値がまとめてある*/
+        HashSet<int> result = RandomNumbers(6, 1, 44);
+
+        // 表示するメソッドを呼び出す
+        ShowNumbers(result);
+    }
+
+    // ランダムな重複しない数値を生成するメソッド
+    static HashSet<int> RandomNumbers(int count, int min, int max)
+    {//ランダムな数値を生成
         Random ran = new Random();
-        for (int i = 0; i < 6; i++)
+        //重複しない数字
+        HashSet<int> result = new HashSet<int>();
+
+        while (result.Count < count)
         {
-            int number = ran.Next(1, 44);
-            Console.WriteLine(number);
+            // 1～43
+            int randomNumber = ran.Next(min, max);
+            // HashSetで重複防止
+            result.Add(randomNumber); 
+        }
+
+        return result;
+    }
+
+    // 数値を表示するメソッド
+    static void ShowNumbers(HashSet<int> numbers)
+    {
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
         }
     }
 }
